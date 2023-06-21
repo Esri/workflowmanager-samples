a50c0af93245c88affd9904f09c895291585a907
Angel Delgadillo <ang10828@esri.com>
2023-06-21 11:13:04 -0700

    Updates for https://devtopia.esri.com/ArcGISPro/ps-workflow-manager/issues/948

---
+++ ServiceBased/ProAddIn/WorkflowManagerSampleAddIn/JobStatistics.cs
@@ -0,0 +1,53 @@
+﻿using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using ArcGIS.Desktop.Workflow.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkflowManagerSampleAddIn
+{
+    internal class JobStatistics : Button
+    {
+        protected override void OnClick()
+        {
+            // Get the Workflow Manager server url
+            var jobsManager = WorkflowClientModule.JobsManager;
+            var jobId = "HSoSXIeFSmu3nuV4rrEzsA";
+
+            QueuedTask.Run(() =>
+            {
+                try
+                {
+                    var jobStats = jobsManager.CalculateJobStatistics(new ArcGIS.Desktop.Workflow.Client.Models.JobStatisticsQuery()
+                    {
+                        Q = "closed=0"
+                    });
+                    var title = "Getting the Job information and properties";
+                    var msg = $"\nJob Statistics after searching are modeled as: {jobStats}\n\n For Example, jobStats.Total = {jobStats.Total}";
+                    MessageBox.Show(msg, title);
+                }
+                catch (Exception ex)
+                {
+                    var title = "Failed to get the job information";
+                    var msg = $"\nJobId: {jobId}\n"
+                        + $"\nError: {ex.Message}";
+                    MessageBox.Show(msg, title);
+                }
+            });
+
+        }
+    }
+}
---
+++ ServiceBased/ProAddIn/WorkflowManagerSampleAddIn/SearchJobs.cs
@@ -0,0 +1,52 @@
+﻿using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using ArcGIS.Desktop.Workflow.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkflowManagerSampleAddIn
+{
+    internal class SearchJobs : Button
+    {
+        protected override void OnClick()
+        {
+            // Get the Workflow Manager server url
+            var jobsManager = WorkflowClientModule.JobsManager;
+            var jobId = "HSoSXIeFSmu3nuV4rrEzsA";
+
+            QueuedTask.Run(() =>
+            {
+                try
+                {
+                    var searchResult = jobsManager.SearchJobs(
+                        new ArcGIS.Desktop.Workflow.Client.Models.SearchQuery()
+                        {
+                            Q = "closed=0"
+                        });
+                    var title = "Getting the Job information and properties";
+                    var msg = $"Search Results are modeled as:\n {searchResult} \n\n For example, searchResult.Num: {searchResult.Num}";
+                    MessageBox.Show(msg, title);
+                }
+                catch (Exception ex)
+                {
+                    var title = "Failed to get the job information";
+                    var msg = $"\nJobId: {jobId}\n" + $"\nError: {ex.Message}";
+                    MessageBox.Show(msg, title);
+                }
+            });
+        }
+    }
+}
